3868e1e87ccc5f8ff1fe7aa5947ad5e989465730
Drikshnemkul <drikshnanem@gmail.com>
2023-12-02 10:43:27 +0545

    day6 completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
+import axios from "axios";
+
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    const response = await axios.get(
+      "https://656aa595dac3630cf7273294.mockapi.io/products"
+    );
+    setProducts(response.data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  console.log(products);
   return (
     <>
       <Navbar />
-      <div className="card">
-        <img src="https://via.placeholder.com/300" alt="Product Image" />
-        <div className="card-content">
-          <div className="product-name">Product Name</div>
-          <div className="product-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      {products.map((product) => {
+        return (
+          <div key={product.id} className="card">
+            <img src={product.productImage} alt="product image" />
+            <h2 className="Product-name">{product.productName}</h2>
+            <p className="product-description">{product.productDescription}</p>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </>
   );
 };
